d113bfca505ed05aebe504f2a28fb35b0ed7921f
manueldavid123 <manueldavid123@gmail.com>
2017-10-03 17:22:09 +0200

    Update HttpRedisCache.php

--- HttpRedisCache.php
+++ HttpRedisCache.php
@@ -5,36 +5,28 @@
  * Date: 5/03/14
  * Time: 15:46
  */
-
 namespace Solilokiam\HttpRedisCache;
-
-
 use Solilokiam\HttpRedisCache\Store\RedisHttpStore;
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
-
 class HttpRedisCache extends HttpCache
 {
     public function createStore()
     {
-        return new RedisHttpStore($this->getConnectionParams(), $this->getDigestKeyPrefix(), $this->getLockKey(
-        ), $this->getMetadataKeyPrefix());
+        return new RedisHttpStore($this->getConnectionParams(), $this->getOptions(), $this->getDigestKeyPrefix(
+        ), $this->getLockKey(), $this->getMetadataKeyPrefix());
     }
-
     public function getConnectionParams()
     {
         return array('host' => 'localhost');
     }
-
     public function getDigestKeyPrefix()
     {
         return 'hrd';
     }
-
     public function getLockKey()
     {
         return 'hrl';
     }
-
     public function getMetadataKeyPrefix()
     {
         return 'hrm';
